7eb810e6500c280abc99fd03d72b74ac7268bbce
yakinika <ndaroui@colostate.edu>
2023-04-06 21:26:18 -0700

    modify
    removed exit() next to when release script and moved to the controller script to assets

---
+++ Unity_Project/Assets/Controller.cs
@@ -0,0 +1,14 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    public void Exit()
+    {
+        Debug.Log($"Exiting Game...");
+        Application.Quit();
+
+    }
+}
