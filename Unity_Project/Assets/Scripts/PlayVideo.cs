e9f624fd75ce598f39a9ab72e30f537e577deab8
yakinika <ndaroui@colostate.edu>
2023-04-10 16:58:20 -0700

    next button will wait
    - Next button will wait till video is over before playing for VOT training videos
    - next button is disabled

--- Unity_Project/Assets/Scripts/PlayVideo.cs
+++ Unity_Project/Assets/Scripts/PlayVideo.cs
@@ -5,9 +5,23 @@ using UnityEngine.Video;
 public class PlayButton : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
+    private bool videoWasPlayed = false;
+    public GameObject button;
 
     public void PlayVideo()
     {
         videoPlayer.Play();
+    }
+
+    void Update()
+    {
+        if (videoPlayer.isPlaying && !videoWasPlayed)
+        {
+            videoWasPlayed = true;
+        }
+        else if (!videoPlayer.isPlaying && videoWasPlayed)
+        {
+            button.SetActive(true);
+        }
     }
 }
