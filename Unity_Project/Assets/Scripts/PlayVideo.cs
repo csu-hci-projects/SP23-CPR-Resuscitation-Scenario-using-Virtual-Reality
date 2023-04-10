65c8f5e8b543177c359635ddc535a5284cc9c943
Matt <matthewcmattson@gmail.com>
2023-04-10 14:39:11 -0600

    Extro/Recap Aligned/Linked
    - Realigned and positioned recap/extro Panel
    - Added new script to intro/extro for next button to show after video is complete

--- Unity_Project/Assets/Scripts/PlayVideo.cs
+++ Unity_Project/Assets/Scripts/PlayVideo.cs
@@ -5,9 +5,26 @@ using UnityEngine.Video;
 public class PlayButton : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
+    private bool videoWasPlayed = false;
+    public GameObject button;
+
+    void Update()
+    {
+        if (videoPlayer.isPlaying && !videoWasPlayed)
+        {
+            videoWasPlayed = true;
+        }
+        else if (!videoPlayer.isPlaying && videoWasPlayed)
+        {
+            button.SetActive(true);
+        }
+    }
 
     public void PlayVideo()
     {
+        videoWasPlayed = true;
         videoPlayer.Play();
+
     }
+
 }
