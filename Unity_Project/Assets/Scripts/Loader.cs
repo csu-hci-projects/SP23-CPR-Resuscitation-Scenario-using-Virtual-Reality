7239f2c5055379c6c3de9cd2d3caa92ae4d2207a
yakinika <ndaroui@colostate.edu>
2023-04-11 14:19:03 -0700

    restart and exit VR fixed
    - created an exit VR object, and attached it to the QuitVR
    - added to the QuitVR script to turn off VR
    - created a restart object that has the restart script and attached it to restart button
    -fixed both buttons

---
+++ Unity_Project/Assets/Scripts/Loader.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    public void QuitVR()
+    {
+        UnityEngine.XR.XRSettings.enabled = false;
+        SceneManager.LoadScene("PostAssesment/PostAssesment");
+    }
+
+    public void LoadVR()
+    {
+        SceneManager.LoadScene("CPR-Module");
+    }
+
+}
