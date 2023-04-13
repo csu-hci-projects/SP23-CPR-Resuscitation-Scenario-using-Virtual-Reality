fa8250815e0247b24fedd939a85c14ff6a0f0005
Matt <matthewcmattson@gmail.com>
2023-04-13 00:13:35 -0600

    Test Option/Script

--- Unity_Project/Assets/Scripts/SceneSwitcher.cs
+++ Unity_Project/Assets/Scripts/SceneSwitcher.cs
@@ -40,6 +40,10 @@ public class SceneSwitcher : MonoBehaviour
         if (pid == "0") {
             Debug.Log("The value of pid is 0");
             SceneManager.LoadScene(33);
+        }
+        else if (pid == "Fake") {
+            Debug.Log("The value of pid is Fake");
+            SceneManager.LoadScene(36);
         } else {
             Debug.Log("The value of pid is NOT 0");
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
